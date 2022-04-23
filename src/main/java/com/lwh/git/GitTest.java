e744169db49e09e5159b9efdd28fa4b47c09c0ec
LWH <1803057925@qq.com>
2022-04-23 20:13:49 +0800

    First Commit

---
+++ src/main/java/com/lwh/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.lwh.git;
+
+/**
+ * @ClassName GitTest
+ * @Description TODO
+ * @Author Lin
+ * @Date 2022/4/23 20:09
+ * @Version 1.0
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
---
+++ src/main/java/com/lwh/git/Hello.java
@@ -0,0 +1,14 @@
+package com.lwh.git;
+
+/**
+ * @ClassName Hello
+ * @Description TODO
+ * @Author Lin
+ * @Date 2022/4/23 20:10
+ * @Version 1.0
+ **/
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
