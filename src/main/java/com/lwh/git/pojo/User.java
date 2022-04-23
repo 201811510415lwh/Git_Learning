3823ab91479fff629474fb2c3cf4834c2da18a7b
LWH <1803057925@qq.com>
2022-04-23 20:58:03 +0800

    user Commit1

--- src/main/java/com/lwh/git/pojo/User.java
+++ src/main/java/com/lwh/git/pojo/User.java
@@ -8,4 +8,40 @@ package com.lwh.git.pojo;
  * @Version 1.0
  **/
 public class User {
+    private String name;
+    private Integer age;
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", address='" + address + '\'' +
+                '}';
+    }
 }
