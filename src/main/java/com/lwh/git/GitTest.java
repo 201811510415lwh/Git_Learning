927e4dbaeb95d811c3132febb1fa938e70364b6f
LWH <1803057925@qq.com>
2022-04-23 20:19:26 +0800

    Third Commit

--- src/main/java/com/lwh/git/GitTest.java
+++ src/main/java/com/lwh/git/GitTest.java
@@ -10,10 +10,14 @@ package com.lwh.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
-        System.out.println("hello git1");
+         System.out.println("hello git1");
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hello git4");
         System.out.println("hello git5");
+        System.out.println("hello git6");
+        System.out.println("hello git7");
+        System.out.println("hello git8");
+        System.out.println("hello git9");
     }
 }
--- src/main/java/com/lwh/git/Hello.java
+++ src/main/java/com/lwh/git/Hello.java
@@ -10,5 +10,10 @@ package com.lwh.git;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world2");
+        System.out.println("hello world3");
+        System.out.println("hello world4");
+        System.out.println("hello world5");
+        System.out.println("hello world6");
     }
 }
